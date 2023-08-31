e821bdca47472ccf1e7468e3ad8fd4d17eac3d67
BrankoRF <142909215+BrankoRF@users.noreply.github.com>
2023-08-31 14:04:40 +0200

    Add files via upload

---
+++ ListVM_ClusterHosts.ps1
@@ -0,0 +1,28 @@
+﻿# Povezivanje na VMware vCenter
+Connect-VIServer -Server <vCenterServer> -User <username> -Password <password>
+
+# Ime datoteke za zapis rezultata
+$outputFile = "vm_host_clusters.txt"
+
+# Funkcija za zapisivanje linije u datoteku
+function WriteToOutputFile($line) {
+    $line | Out-File -Append -FilePath $outputFile
+}
+
+# Popis svih klastera
+$clusters = Get-Cluster
+
+foreach ($cluster in $clusters) {
+    WriteToOutputFile "Cluster: $($cluster.Name)"
+
+    # Popis svih VM unutar klastera
+    $vmsInCluster = $cluster | Get-VM
+
+    foreach ($vm in $vmsInCluster) {
+        $hostCluster = ($vm.VMHost | Get-Cluster).Name
+        WriteToOutputFile "  VM: $($vm.Name), Host Cluster: $hostCluster"
+    }
+}
+
+# Odspajanje sa vCenter serverom
+Disconnect-VIServer -Confirm:$false
