bed8054ae37c1c952664d5bb997e5f356507cc08
BrankoRF <142909215+BrankoRF@users.noreply.github.com>
2023-08-31 11:15:15 +0200

    Add files via upload

---
+++ DatastoreVM.ps1
@@ -0,0 +1,38 @@
+﻿# Povezivanje na VMware vCenter
+Connect-VIServer -Server hovcenter.rbj.co.yu -User yuasubr -Password 21S@lakazu21azu
+
+# Ime datoteke za zapis rezultata
+$outputFile = "C:\Temp\DatastoreVM.txt"
+
+# Funkcija za zapisivanje linije u datoteku
+function WriteToOutputFile($line) {
+    $line | Out-File -Append -FilePath $outputFile
+}
+
+# Popis svih klastera
+$clusters = Get-Cluster
+
+# Prolazak kroz svaki klaster
+foreach ($cluster in $clusters) {
+    WriteToOutputFile "Cluster: $($cluster.Name)"
+
+    # Popis svih datastore-ova koji pripadaju klasteru
+    $clusterDatastores = $cluster | Get-Datastore
+
+    foreach ($datastore in $clusterDatastores) {
+        WriteToOutputFile "  Datastore: $($datastore.Name)"
+
+        # Popis svih VM na datastore-u
+        $vmsOnDatastore = $datastore | Get-VM
+
+        # Prolazak kroz svaku VM na datastore-u
+        foreach ($vm in $vmsOnDatastore) {
+            if ($vm.Guest.OSFullName) {
+                WriteToOutputFile "    VM: $($vm.Name), OS: $($vm.Guest.OSFullName)"
+            }
+        }
+    }
+}
+
+# Odspajanje sa vCenter serverom
+Disconnect-VIServer -Confirm:$false
---
+++ DatastoreVM_ClusterHostovi.ps1
@@ -0,0 +1,39 @@
+﻿# Povezivanje na VMware vCenter
+Connect-VIServer -Server hovcenter.rbj.co.yu -User yuasubr -Password 21S@lakazu21azu
+
+# Ime datoteke za zapis rezultata
+$outputFile = "C:\Temp\DatastoreVM_ClusterHostovi.txt"
+
+# Funkcija za zapisivanje linije u datoteku
+function WriteToOutputFile($line) {
+    $line | Out-File -Append -FilePath $outputFile
+}
+
+# Popis svih klastera
+$clusters = Get-Cluster
+
+# Prolazak kroz svaki klaster
+foreach ($cluster in $clusters) {
+    WriteToOutputFile "Cluster: $($cluster.Name)"
+
+    # Popis svih datastore-ova koji pripadaju klasteru
+    $clusterDatastores = $cluster | Get-Datastore
+
+    foreach ($datastore in $clusterDatastores) {
+        WriteToOutputFile "  Datastore: $($datastore.Name)"
+
+        # Popis svih VM na datastore-u
+        $vmsOnDatastore = $datastore | Get-VM
+
+        # Prolazak kroz svaku VM na datastore-u
+        foreach ($vm in $vmsOnDatastore) {
+            if ($vm.Guest.OSFullName) {
+                $hostCluster = ($vm.VMHost | Get-Cluster).Name
+                WriteToOutputFile "    VM: $($vm.Name), OS: $($vm.Guest.OSFullName), Host Cluster: $hostCluster"
+            }
+        }
+    }
+}
+
+# Odspajanje sa vCenter serverom
+Disconnect-VIServer -Confirm:$false
