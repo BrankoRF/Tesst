7d27b8fd28bedda517f63d17ddc2e450a5217134
BrankoRF <142909215+BrankoRF@users.noreply.github.com>
2023-10-03 08:43:08 +0200

    Update VM_disk_sizes.ps1

--- VM_disk_sizes.ps1
+++ VM_disk_sizes.ps1
@@ -11,7 +11,7 @@ function WriteToOutputFile($line, $filePath) {
 }
 
 # Popis svih VM-ova, ukoliko se izbrise linija -Name i masine skripta ce raditi na svim masinama upit za proveru velicine diskova
-$virtualMachines = Get-VM -Name "HOEBPPDF","HOEBPAPP1","PDREP","HOEBPWEB1","HOEBANKWEBSTAT","HOEBPWREPORTING","HOEBANKIN2ROL","HOEBPWEB2","HOEBPADMIN","EBPP2PDF","EBWEBTEST2","EBTAPP1","EBTADMIN","EBPP2APP2","EBTPDF","IBANKRELAY","EBPP2SQL","EBPP2APP1","EBPP2ADMIN","HOROLPKI","DREBPWEB1","DREBPAPP1","EBTWEB1","DRCMS","HOEBPAPP2","EBPP2WEB2","EBPP2WEB1","DREBPAPP2","DREBPWEB2","HOEBPAPP3","HOEBPWEB3","HOEBPWEB5","HOEBPWEB6","HOEBPAPP5","HOEBPAPP6","HOEBPADMIN2","EBTADMIN2","HOEBPAPP4","ebanktsql","HOEBPWEB4","EBPP2APP3","EBPP2WEB3","EBTPDF2","EBINTEGADMIN","EBINTEGADMIN2","EBINTEGPDF","EBINTEGWEB1","EBINTEGAPP1","EBINTEGIN2ROL","ebankppsql","ebanksqlrestore","hoebankreport","hoebnode1","hoebnode2","drebnsql","ebankpp19sql"
+$virtualMachines = Get-VM -Name "virtualmaina ime1, virtualmasina ime2"
 
 # Stvaranje praznog niza za spremanje rezultata
 $results = @()
